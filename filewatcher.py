18d19e648ada38bb99327b35f01f34508c831ab2
natellu <t.le0nh4rdt@gmail.com>
2021-04-27 21:27:18 +0200

    added ffmpeg flac to mp3

--- filewatcher.py
+++ filewatcher.py
@@ -1,19 +1,56 @@
 import sys
+import os
 import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+from ffmpy import FFmpeg
+
+WatchingExtensionsList = [".flac"]
+watchingfolder = "C:\\Users\\natellu\\Documents\\programmierung\\pythonscripts\\flacs\\"
+outputfolder = "C:\\Users\\natellu\\Documents\\programmierung\\pythonscripts\\mp3\\"
+
+class Handler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f'event: {event}')
+       
+        if event.is_directory:
+            print("is directory")
+            return
+
+        filename, file_extension = os.path.splitext(event.src_path)
+        if file_extension not in WatchingExtensionsList:
+            print("not in extention list")
+            return
+
+        file_size = -1
+        while file_size != os.path.getsize(event.src_path):
+            file_size = os.path.getsize(event.src_path)
+            time.sleep(1)
+
+        print("done copying")
+
+        ff = FFmpeg(
+            inputs={event.src_path: None},
+            outputs={'folder\\output.mp3': "-c:a libmp3lame -b:a 320k"}
+        )
+       
+        ff.run()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    path = watchingfolder
+    """ event_handler = LoggingEventHandler() """
+    event_handler = Handler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
+
     print("Watching " + path)
+
     try:
         while True:
             time.sleep(1)
